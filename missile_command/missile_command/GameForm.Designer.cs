0df472a07ebc6e4499773230575c2edf83ef5376
BrettRegnier <bretternestregnier@gmail.com>
2017-12-08 19:13:35 -0700

    Converting into a while loop instead of a timer

--- missile_command/missile_command/GameForm.Designer.cs
+++ missile_command/missile_command/GameForm.Designer.cs
@@ -34,6 +34,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
 			this.ClientSize = new System.Drawing.Size(600, 343);
 			this.Name = "GameForm";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
--- missile_command/missile_command/GameForm.cs
+++ missile_command/missile_command/GameForm.cs
@@ -6,20 +6,15 @@ using System.Drawing;
 using System.Text;
 using System.Threading;
 using System.Windows.Forms;
-using Microsoft.DirectX;
-using Microsoft.DirectX.Direct3D;
 using System.Drawing;
 
 namespace missile_command
 {
 	public partial class GameForm : Form
 	{
-		Microsoft.DirectX.Direct3D.Device device;
-		Microsoft.DirectX.DirectDraw.Surface surface;
-
 		int fps = 0, frames = 0;
-		long timeStarted = Environment.TickCount;
-		Graphics g;
+		long tickCount = Environment.TickCount;
+		long elapsedTime = 0;
 
 		List<GameObject> objectList = new List<GameObject>();
 
@@ -31,87 +26,66 @@ namespace missile_command
 		}
 		private void Main()
 		{
-			InitDevice();
+			InitGame();
 
 			// Unit tests?
-			//objectList.Add(GameObjectFactory.MakeBomb(new Point(0, 0), new Point(500, 1080), new Dimensions(1, 1), PType.ENEMY));
-			//this.Invalidate();
+			objectList.Add(GameObjectFactory.MakeBomb(new Point(0, 0), new Point(500, 1080), new Dimensions(1, 1), PType.ENEMY));
 		}
-		private void InitForm()
+		private void InitGame()
 		{
+			SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+			SetStyle(ControlStyles.ResizeRedraw, true);
+			SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+			SetStyle(ControlStyles.UserPaint, true);
+
 			// Maximize and hide everything else
-			this.StartPosition = FormStartPosition.Manual;
+			StartPosition = FormStartPosition.Manual;
+			FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+
 			int height = Screen.PrimaryScreen.Bounds.Height;
 			int width = Screen.PrimaryScreen.Bounds.Width;
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			this.ClientSize = new Size(width, height);
+			ClientSize = new Size(width, height);
 		}
-		private void InitDevice()
+		private void Update()
 		{
-			PresentParameters pp = new PresentParameters();
-			pp.Windowed = true;
-			pp.SwapEffect = SwapEffect.Discard;
-			//pp.DeviceWindow.Width = Screen.PrimaryScreen.Bounds.Width;
-			//pp.DeviceWindow.Height = Screen.PrimaryScreen.Bounds.Height;
-
-			device = new Device(0, DeviceType.Hardware, this, CreateFlags.HardwareVertexProcessing, pp);
-		}
-		private void StartThread()
-		{
-
-		}
-		private void Play()
-		{
-			device.Clear(ClearFlags.Target, Color.Black, 0, 1);
-
-			surface.DrawEllipse(10, 10, 10, 10);
-
-			device.Present();
-
-			/*
 			while (true)
 			{
-					if (frames >= 60)
-					{
-							if (Environment.TickCount >= timeStarted)
-							{
-									Console.Out.WriteLine(Environment.TickCount - timeStarted);
-									Console.Out.WriteLine("Frames: " + frames);
-									fps = frames;
-									frames = 0;
-									timeStarted = Environment.TickCount;
-									break;
-							}
-					}
-					frames++;
+				if (Environment.TickCount >= elapsedTime + 1000)
+				{
+					fps = frames;
+					frames = 0;
+					elapsedTime = Environment.TickCount;
+					Console.Out.WriteLine("Frames: " + fps);
+				}
+				if (Environment.TickCount >= tickCount + 15)
+				{
+					tickCount = Environment.TickCount;
 					this.Invalidate();
+					break;
+				}
 			}
-			//Console.Out.WriteLine(fps);*/
 		}
 		private void GameForm_Paint(object sender, PaintEventArgs e)
 		{
-			Play();
-			/*
-			//https://www.youtube.com/watch?v=AC9xeiiSD-o
-			// This is the Frames thing that i am trying to get to work
-
-			// Use directX and threading to elimate this.
-			//https://www.youtube.com/watch?v=M4IywpwxdKg&list=PLizgKJ9dhxgwwSFCCLI8bqOrpEFCRcS9U
 			try
 			{
-				g = e.Graphics;
-
 				for (int i = 0; i < objectList.Count; i++)
 				{
-					objectList[i].Draw(g);
+					((Bomb)objectList[i]).Move();
+					objectList[i].Draw(e.Graphics);
 				}
-				Play();
+
+
+				
+				frames++;
+
+				// Update after drawing
+				Update();
 			}
 			catch (Exception ex)
 			{
 				Close();
 			}
-			*/
 		}
 	}
 }
