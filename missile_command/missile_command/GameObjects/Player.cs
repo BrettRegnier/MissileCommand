a9a84a8de3e494773da87c9f69153e67ffda7199
BrettRegnier <bretternestregnier@gmail.com>
2017-12-08 19:20:22 -0700

    Testing branching

--- missile_command/missile_command/GameObjects/Player.cs
+++ missile_command/missile_command/GameObjects/Player.cs
@@ -5,23 +5,24 @@ using System.Text;
 
 namespace missile_command
 {
-    class Player : GameObject
-    {
-        private Reticle cursor;
+	class Player : GameObject
+	{
+		private Reticle cursor;
 
-        public Player(Point pos, Dimensions dim, Graphics g, PType p) : base(pos, dim, p)
-        {
-            cursor = new Reticle(g, p);
-        }
+		public Player(Point pos, Dimensions dim, Graphics g, PType p) : base(pos, dim, p)
+		{
+			cursor = new Reticle(g, p);
+		}
 
-        public override void Draw(Graphics g)
-        {
-            throw new NotImplementedException();
-        }
+		public override void Draw(Graphics g)
+		{
+			// Need to calculate the circle size, and the line, with the angle based on the aming reticle
+			throw new NotImplementedException();
+		}
 
-        public override void Collided()
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public override void Collided()
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
