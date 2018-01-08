9f08f0027ff0dc7a9e2ade465f07124c2a07d132
BrettRegnier <bretternestregnier@gmail.com>
2018-01-07 18:07:46 -0700

    chore: organize classes into different folders

---
+++ missile_command/src/game/_GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace missile_command
+{
+	class GameManager
+	{
+		// TODO move all of the game form game code to here.
+	}
+}
